afcc36b81cbbdbb0ab191370a4e5942017ed0077
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-08-10 20:46:53 +0900

    お題保存機能追加

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -2,4 +2,21 @@ class ThemesController < ApplicationController
   def index
     @new_theme = Theme.new
   end
+
+  def create
+    @new_theme = Theme.new(create_params)
+    if @new_theme.save
+      redirect_to theme_path, notice: "お題の作成に成功しました"
+    else
+      redirect_to root_path, alert:"お題の作成に失敗しました"
+    end
+  end
+
+  def show
+  end
+
+  private
+    def create_params
+      params.require(:theme).permit(:red, :blue).merge(user_id: current_user.id)
+    end
 end
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,12 @@
 class Theme < ApplicationRecord
   has_many :opinions
   belongs_to :user
+  validate :add_error_sample
+
+  def add_error_sample
+    if red.blank? || blue.blank?
+      errors[:red] << "お題は必ず両方入力して下さい"
+      errors[:blue] << "お題は必ず両方入力して下さい"
+    end
+  end
 end
