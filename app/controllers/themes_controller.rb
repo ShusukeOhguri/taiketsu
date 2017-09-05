b42bb11c3b95b9fa2856ac16dfc5218b9de2d97b
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-09-05 18:27:47 +0900

    テーマ投稿機能作成

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,22 +1,28 @@
 class ThemesController < ApplicationController
+  before_action :new_theme, only: [:index, :show]
+
   def index
-    @new_theme = Theme.new
   end
 
   def create
     @new_theme = Theme.new(create_params)
     if @new_theme.save
-      redirect_to theme_path, notice: "お題の作成に成功しました"
+      redirect_to theme_path(@new_theme.id), notice: "お題の作成に成功しました"
     else
-      redirect_to root_path, alert:"お題の作成に失敗しました"
+      redirect_to :back, alert:"お題の作成に失敗しました"
     end
   end
 
   def show
+    @theme = Theme.find(params[:id])
   end
 
   private
     def create_params
       params.require(:theme).permit(:red, :blue).merge(user_id: current_user.id)
+    end
+
+    def new_theme
+      @new_theme = Theme.new
     end
 end
