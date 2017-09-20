945a295bb37816e9366fbeefc84cd920197fc8b6
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-09-21 06:24:07 +0900

    お題の表示機能編集

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -15,6 +15,7 @@ class ThemesController < ApplicationController
 
   def show
     @theme = Theme.find(params[:id])
+    @opinions = @theme.opinions
     @user_id = current_user.id
     @new_opinion = Opinion.new
   end
@@ -26,5 +27,38 @@ class ThemesController < ApplicationController
 
     def new_theme
       @new_theme = Theme.new
+      @popularity_themes = choise_theme(0, 12)
+      @recruiting_opinions_themes = choise_theme(1, 6)
+    end
+
+    def choise_theme(sort, cnt_lmt)
+      choise_themes = []
+      Theme.ids.each do |theme|
+        choise_themes << {id:theme, opinion:Theme.find(theme).opinions.count}
+      end
+
+      if sort == 0
+        sorted_themes = choise_themes.sort{|a, b| b[:opinion] <=> a[:opinion]}.take(cnt_lmt)
+      elsif sort == 1
+        sorted_themes = choise_themes.sort{|a, b| a[:opinion] <=> b[:opinion]}.take(cnt_lmt)
+      end
+
+      chosen_themes = []
+      sorted_themes.each do |theme|
+        cnt_red = 0
+        cnt_blue = 0
+        opinions = Theme.find(theme[:id]).opinions
+        opinions.each do |opinion|
+          if opinion[:side] == 0
+            cnt_red += 1
+          elsif opinion[:side] == 1
+            cnt_blue += 1
+          end
+        end
+        theme[:red] = cnt_red
+        theme[:blue] = cnt_blue
+        chosen_themes << theme
+      end
+      return chosen_themes
     end
 end
